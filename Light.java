252c162a63408fb4c3305496bfe69748d8c703e7
chalitha udara <chalithaudara@gmail.com>
2012-01-18 21:31:26 +0530

    first commit

---
+++ Light.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author ChalithaUdara
+ * 
+ */
+public interface Light {
+
+	public abstract void turnON();
+
+	public abstract void turnOff();
+
+	public abstract String getColor();
+}
